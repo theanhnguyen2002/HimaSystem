9b5c23411fbd3084280157f5171f5aab57a5d81f
theanhnguyen <theanhnguyen>
2024-08-15 11:29:44 +0700

    update

---
+++ client/src/layout/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='text-red-700'>
+          header
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Home page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
